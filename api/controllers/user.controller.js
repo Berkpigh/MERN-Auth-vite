c83a82c7e045ada862066b6dbb20a77560dbac72
Pierre Berkovits <berkovitspierre@gmail.com>
2024-02-10 22:00:54 +0100

    create update user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -10,6 +10,37 @@ export const test = (req, res) => {
 
 // update user
 export const updateUser = async (req, res, next) => {
+    console.log(req.user.id)
+    if (req.user.id !== req.params.id) {
+      return next(errorHandler(401, 'You can update only your account!'));
+    }
+    try {
+      if (req.body.password) {
+        req.body.password = bcryptjs.hashSync(req.body.password, 10);
+      }
+  
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: {
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            profilePicture: req.body.profilePicture,
+          },
+        },
+        { new: true }
+      );
+      const { password, ...rest } = updatedUser._doc;
+      res.status(200).json(rest);
+    } catch (error) {
+      next(error);
+    }
+  };
+  
+
+
+/* export const updateUser = async (req, res, next) => {
     if (req.user.id !== req.params.id) {
         return next(errorHandler(401, 'You can update only your account!'))
     }
@@ -34,4 +65,4 @@ export const updateUser = async (req, res, next) => {
     } catch (error) {
         next(error)
     }
-}
+} */
