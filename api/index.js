4e31bbacd73c2c8dcd4c1c9ce9d67b25624e85c1
Pierre Berkovits <berkovitspierre@gmail.com>
2024-02-10 22:18:37 +0100

    fix pprocess bug in verifyUser

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import cookieParser from 'cookie-parser'
 
 dotenv.config()
 
+console.log(process.env.MONGO)
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to MongoDB')
 }).catch((err) => {
