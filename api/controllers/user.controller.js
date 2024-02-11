73619203ca32fb807417cb78ac69c8624f953229
Pierre Berkovits <berkovitspierre@gmail.com>
2024-02-11 15:43:21 +0100

    add delete user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -10,7 +10,6 @@ export const test = (req, res) => {
 
 // update user
 export const updateUser = async (req, res, next) => {
-    console.log(req.user.id)
     if (req.user.id !== req.params.id) {
       return next(errorHandler(401, 'You can update only your account!'));
     }
@@ -37,6 +36,19 @@ export const updateUser = async (req, res, next) => {
       next(error);
     }
   };
+
+// delete user
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(401, 'You can delete only your account!'));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.id)
+    res.status(200).json('User has been deleted!')
+  } catch (error) {
+    next(error)
+  }
+}
   
 
 
